5049832dc24a22541161565011ee7aa08c639941
ghadi1998 <51037635+ghadi1998@users.noreply.github.com>
2019-06-28 15:10:28 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -52,7 +52,6 @@ app.use('/api/login' ,userRoute.login)
 
 
 
-fetch("http://localhost:3001/api/login", {"credentials":"omit","headers":{"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3","accept-language":"en-GB,en;q=0.9,en-US;q=0.8,ar;q=0.7,pt;q=0.6","cache-control":"max-age=0","if-none-match":"W/\"6cc-gCE7WmwumEcbJBh2aiQCo0ieb7c\"","upgrade-insecure-requests":"1"},"referrerPolicy":"no-referrer-when-downgrade","body":req.body,"method":"GET","mode":"cors"}); ;
 
 
 
